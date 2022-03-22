155f4598ab6535947146fad1cc35fde0d1d4b868
Connorjaz <connoraz141@gmail.com>
2022-03-22 18:41:40 -0400

    minor tweaks

--- src/components/Founders.tsx
+++ src/components/Founders.tsx
@@ -2,15 +2,15 @@ import '../css/Founders.css';
 const Founders = () => {
     return (
         <div className="founder-container">
-            <h3>Meet the Designers</h3>
-            <p>“Good programmers do programming not because they expect to<br />get paid or get adulation by the public, but because it is fun to program.”<br />— Linus Torvalds, software engineer, inventor of the Linux operating system</p>
+            <h3>The Founders</h3>
+            <p>“Good programmers do programming not because they expect to<br/>get paid or get adulation by the public, but because it is fun to program.”<br />— Linus Torvalds, Software Engineer</p>
             <div className="founders">
                 <div className="founder">
-                    <img src={require("../assets/cosmiq.png")} alt="alternative" />
+                    <img src={require("../assets/shocked.png")} alt="alternative" />
                     <h6>Connor Aleks<br />Frontend Lead</h6>
                 </div>
                 <div className="founder">
-                    <img src={require("../assets/lemonhead.jpg")} alt="alternative" />
+                    <img src={require("../assets/cosmiq.png")} alt="alternative" />
                     <h6>Cole Peterson<br />Backend Lead</h6>
                 </div>
             </div>
